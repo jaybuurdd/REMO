19929d8849d957935706b2e4d7386495ca9e9dd5
jaybuurdd <ec2-user@ip-172-31-29-184.ec2.internal>
2024-01-02 06:24:58 +0000

    db config update [From EC2]

--- services/database.py
+++ services/database.py
@@ -10,13 +10,19 @@ load_dotenv()
 def config_rds_db():
     logger.info("DB Config Started...")
     global rds
+    print(os.getenv('AWS_POSTGRES_DB_NAME'))
+    print(os.getenv('AWS_POSTGRES_MASTER_USERNAME'))
+    print(os.getenv('AWS_POSTGRES_MASTER_PASSWORD'))
+    print(os.getenv('AWS_POSTGRES_HOST'))
+
     # connect to RDS db
     conn = psycopg2.connect(
         dbname=os.getenv('AWS_POSTGRES_DB_NAME'),
         user=os.getenv('AWS_POSTGRES_MASTER_USERNAME'),
         password=os.getenv('AWS_POSTGRES_MASTER_PASSWORD'),
         host=os.getenv('AWS_POSTGRES_HOST'),
-        port="5432"
+        port="5432",
+        sslmode="require"
     )
 
     cursor = conn.cursor()
