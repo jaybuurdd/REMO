63ab752ed7d53859757342c084b0744ce793fec1
jaybuurdd <ec2-user@ip-172-31-29-184.ec2.internal>
2024-01-03 04:42:19 +0000

    modified env variable for openai api [From EC2]

--- repo/review.py
+++ repo/review.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv('OPEN_AI_API_KEY')
+api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=api_key)
 
 
